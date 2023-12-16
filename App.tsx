e0af8ddd73ffc4d3e5a7c607aa69fb6f7dc4fb09
chayan chowdhury <chayanchowdhury43@yahoo.com>
2023-12-16 23:58:41 +0530

    Implememed Image Url and Onclick Functiolity

--- App.tsx
+++ App.tsx
@@ -1,118 +1,59 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { Component } from 'react';
+import { View, Text, FlatList, Image, Alert } from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+class App extends Component {
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  myData=[
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+    {
+      title:"A",subTitle:"America",img:"https://picsum.photos/seed/picsum/200/300"
+    },{
+      title:"B",subTitle:"Bangladesh",img:"https://picsum.photos/200/300"
+    },{
+      title:"C",subTitle:"Canada",img:"https://picsum.photos/id/237/200/300"
+    },{
+      title:"D",subTitle:"Denmark",img:"https://picsum.photos/200/300/?blur=2"
+    },{
+      title:"E",subTitle:"England",img:"https://picsum.photos/200/300/?blur"
+    },{
+      title:"F",subTitle:"France",img:"https://picsum.photos/200/300.webp"
+    },{
+      title:"G",subTitle:"Germany",img:"https://picsum.photos/200/300.jpg"
+    },{
+      title:"H",subTitle:"Holand",img:'https://picsum.photos/seed/picsum/200/300'
+    },{
+      title:"I",subTitle:"India",img:"https://picsum.photos/200/300?grayscale"
+    }
+  ];
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+  onItemClick=(alertTitle)=>{
+    Alert.alert(alertTitle)
+  }
+
+  ChildView=({childTitle,childSubtitle,img})=>{
+
+    return (<View style={{flexDirection:'row',backgroundColor:'gray',padding:5,margin:5,borderRadius:10,flex:100}}>
+
+      <View style={{flex:20}}>
+      <Image  style={{height:70,width:"100%",padding:16,borderRadius:10}}source={{uri:img}}/>
+      </View>
+
+      <View style={{flex:80,padding:10}}>
+
+      <Text  onPress = {this.onItemClick.bind(this,childSubtitle)}style={{color:'black',fontSize:20}}>{childTitle}</Text>
+      <Text style={{color:'black',fontSize:18}}>{childSubtitle}</Text>
+
+      </View>
+
+     
+    </View>)
+
+  }
+
+  render() {
+    return (
+      <FlatList data={this.myData} renderItem={({item})=><this.ChildView childTitle={item.title} childSubtitle={item.subTitle} img={item.img}/>}></FlatList>
+    );
+  }
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
